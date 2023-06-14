82487b7be636b5c36e4934126d017f848ed22b09
Rob <rrojasj@ucenfotec.ac.cr>
2023-06-13 23:41:17 -0600

    Black Jack game structure and code environment

---
+++ Code/black_jack_functions.py
@@ -0,0 +1,18 @@
+
+
+# 1st Deliverable
+
+def deck_construction():
+    return
+
+def get_card():
+    return
+
+def show_cards():
+    return
+
+def get_as_value():
+    return
+
+def define_points_dist():
+    return
