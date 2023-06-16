fc1382f850b232771f05d955623ef2f7bbe12714
Rob <rrojasj@ucenfotec.ac.cr>
2023-06-16 09:31:48 -0600

    Diagrams folder and minor changes

---
+++ Code/test.py
@@ -0,0 +1,25 @@
+# a list of all the suits
+
+# "\u2663" = Clubs
+# "\u2665" = Heart
+# "\u2666" = Diamonds
+# "\u2660" = Spades
+
+# Suits = ["\u2663", "\u2665",
+#          "\u2666", "\u2660"]
+# # a list of all the ranks
+# Ranks = ['A', '2', '3', '4', '5',
+#          '6', '7', '8', '9', '10',
+#          'J', 'Q', 'K']
+  
+# # Matching all the suits with all the ranks
+# for rank in Ranks:
+  
+#     for suit in Suits:
+#         print(f'{rank} of {suit}'.ljust(10), end='')
+  
+#     # Spacer
+#     print()
+
+# *- -------------------------------------------------- -*
+
