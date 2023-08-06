1fcdcd8377589c3e4a398a2c36c70e4ef2e24d9d
Roberto Rojas <robjimn@amazon.com>
2023-08-06 14:30:48 -0600

    3era entrega completa. Faltan pruebas para confirmar.

--- Code/black_jack_functions.py
+++ Code/black_jack_functions.py
@@ -1,4 +1,6 @@
 import random
+import os
+from datetime import datetime
 
 jug1 = "Jugador 1"
 jug2 = "Jugador 2"
@@ -26,16 +28,18 @@ def crear_baraja() -> list:
 
     return baraja
 
-def print_baraja(baraja):
+def print_baraja(baraja: list):
     """
     función: print_baraja()
     descripción: Imprime la baraja con sus elementos actuales
     params: baraja
     """
     for i in range(0, len(baraja), 4):
-     print(', '.join(baraja[i:i+4]))
 
-def convert_ace(mano_jug:list) -> list:
+        print(', '.join(map(str, baraja[i:i+4]))) # With this option "map" converts each element to a string
+        #  print(', '.join(baraja[i:i+4])) - This option reads only the strings
+
+def convert_ace(mano_jug:list, jugador: str, dir_bitacora: str) -> list:
     """
     función: convert_as()
     descripción: Convierte el valor actual de la mano en el valor que indique el usuario
@@ -49,6 +53,9 @@ def convert_ace(mano_jug:list) -> list:
                 mano_jug[index] = 1
             elif decision == "11":
                 mano_jug[index] = 11
+    
+    msj_bitacora = f"{jugador} le ha dado el valor de {mano_jug[index]} a su A's"
+    agregar_accion_bitacora(dir_bitacora, msj_bitacora)
     
     return mano_jug
         
@@ -65,7 +72,7 @@ def sumar_mano(mano_jugador:list) -> int:
         if valor_actual in ["J","Q","K","1"]:
             total_mano += 10
         # elif valor_actual == "A":
-        #     valor_as = convert_as(valor_actual)
+        #     valor_as = convert_ace(valor_actual)
         #     total_mano += valor_as
         else:
             total_mano += int(valor_actual)
@@ -85,49 +92,40 @@ def print_mano(jugador:str, mano_jugador:list):
         cont += 1
     print("")
 
+def calcular_pts_individuales(total_jug, total_adv):
+
+    bj = 21
+    pts_won = 0
+
+    if total_jug == bj:
+        if total_adv == bj:
+            pts_won = 3
+        else:
+            pts_won = 6
+    else:
+        if total_jug >= 17 and total_jug <= 20:
+            if total_adv > 21:
+                pts_won = 2
+            elif total_jug > total_adv:
+                pts_won = 2
+        else:
+            if total_jug < 17:
+                if total_jug > total_adv:
+                    pts_won = 1
+            else:
+                if total_jug > bj:
+                    pts_won = 0
+    return pts_won
+
 def total_pts(total_J1:int, total_J2:int) -> dict:
     """
     función: total_pts()
     descripción: Imprime el mensaje con la mano del jugador
     params: jugador, mano_jugador
     """
-    puntos_J1 = 0
-    puntos_J2 = 0
-    totales = {}
 
-    # Puntos - Jugador 1
-    if total_J1 == 21:
-        if total_J2 == 21:
-            puntos_J1 += 3
-            puntos_J2 += 3
-        else:
-            puntos_J1 += 6
-    elif total_J1 >= 17 and total_J1 <= 20:
-        if total_J1 > total_J2:
-            puntos_J1 += 2
-    elif total_J1 < 17:
-        if total_J1 > total_J2:
-            puntos_J1 += 1
-    elif total_J1 > 21:
-        puntos_J1 += 0
-        puntos_J2 += 6
-    
-    # Puntos - Jugador 2
-    if total_J2 == 21:
-        if total_J1 == 21:
-            puntos_J2 += 3
-            puntos_J1 += 3
-        else:
-            puntos_J2 += 6
-    elif total_J2 >= 17 and total_J2 <= 20:
-        if total_J2 > total_J1:
-            puntos_J2 += 2
-    elif total_J2 < 17:
-        if total_J2 > total_J1:
-            puntos_J2 += 1
-    elif total_J2 > 21:
-        puntos_J2 += 0
-        puntos_J1 += 6
+    puntos_J1 = calcular_pts_individuales(total_J1, total_J2)
+    puntos_J2 = calcular_pts_individuales(total_J2, total_J1)
 
     totales = {
         'pts_J1': puntos_J1,
@@ -136,7 +134,7 @@ def total_pts(total_J1:int, total_J2:int) -> dict:
     
     return totales
 
-def play_J1(baraja) -> dict:
+def play_J1(baraja: list, dir_bitacora: str) -> dict:
     """
     función: play_J1()
     descripción: Proceso de juego de una mano del Jugador 1 
@@ -153,6 +151,17 @@ def play_J1(baraja) -> dict:
         carta_aleatoria = random.randint(0,51)
         if baraja_actual[carta_aleatoria] != 0:
             mano_J1.append(baraja_actual[carta_aleatoria])
+
+            if carta_aleatoria in ["J", "Q", "K"]:
+                # msj_bitacora = jug1 + " - Carta: " + str(baraja_actual[carta_aleatoria]) + "y su valor es '10'.\n"
+                msj_bitacora = f"{jug1} - Carta: '{baraja_actual[carta_aleatoria]}' y su valor es '10'.\n" 
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+            else:
+                # msj_bitacora = msj_bitacora = jug1 + " - Carta: " + str(baraja_actual[carta_aleatoria]) + "y su valor es" + str(baraja_actual[carta_aleatoria])+".\n"
+
+                msj_bitacora = f"{jug1} - Carta: '{baraja_actual[carta_aleatoria]}' y su valor es '{baraja_actual[carta_aleatoria]}.'\n" 
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+            
             baraja_actual[carta_aleatoria] = 0
         
         if len(mano_J1) == 2:
@@ -163,9 +172,12 @@ def play_J1(baraja) -> dict:
     # Verifica si existen A's y si es verdadero reemplaza el valor por el que indique el usuario
     cant_ace = mano_J1.count("A")
     if cant_ace >= 1:
-        mano_J1 = convert_ace(mano_J1)
+        mano_J1 = convert_ace(mano_J1, jug1, dir_bitacora)
 
     total = sumar_mano(mano_J1)
+    msj_bitacora = f"La mano del {jug1} suma un total de {total}\n"
+    agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
     data_J1 = {
         "total_mano_J1": total,
         "baraja_actual": baraja_actual,
@@ -174,7 +186,7 @@ def play_J1(baraja) -> dict:
 
     return data_J1
 
-def play_J2(baraja) -> dict:
+def play_J2(baraja: list, dir_bitacora: str) -> dict:
     """
     función: play_J2()
     descripción: Proceso de juego de una mano del Jugador 2
@@ -186,6 +198,7 @@ def play_J2(baraja) -> dict:
     total = 0
     baraja_actual = baraja
     jug2 = "Jugador 2"
+    cant_zeros = baraja.count(0)
 
     while play_in:
         carta_aleatoria = random.randint(0,51)
@@ -193,6 +206,15 @@ def play_J2(baraja) -> dict:
             ""
         else:
             mano_J2.append(baraja_actual[carta_aleatoria])
+            
+            if carta_aleatoria in ["J", "Q", "K"]:
+                msj_bitacora = f"{jug2} - Carta: '{baraja_actual[carta_aleatoria]}' y su valor es '10'.\n" 
+
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+            else:
+                msj_bitacora = f"{jug2} - Carta: '{baraja_actual[carta_aleatoria]}' y su valor es '{baraja_actual[carta_aleatoria]}.'\n" 
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
             baraja_actual[carta_aleatoria] = 0
         
         if len(mano_J2) == 2:
@@ -203,9 +225,12 @@ def play_J2(baraja) -> dict:
     # Verifica si existen A's y si es verdadero reemplaza el valor por el que indique el usuario
     cant_aces = mano_J2.count('A')
     if cant_aces >= 1:
-        mano_J2 = convert_ace(mano_J2)
+        mano_J2 = convert_ace(mano_J2, jug2, dir_bitacora)
 
     total = sumar_mano(mano_J2)
+    msj_bitacora = f"La mano del {jug2} suma un total de {total}\n"
+    agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
     data_J2 = {
         "total_mano_J2": total,
         "baraja_actual": baraja_actual,
@@ -215,15 +240,16 @@ def play_J2(baraja) -> dict:
 
     return data_J2
 
-def jugar_extra(dict_J1:dict) -> dict:
-
+def jugar_extra_1(dict_J1:dict, dir_bitacora: str) -> dict:
     """
-    función: jugar_extra()
-    descripción: 
-    params: baraja
+    función: jugar_extra_1()
+    descripción: Jugar una carta extra del jugador 1
+    params: dict_J1
     """
     baraja_actual = dict_J1["baraja_actual"]
     carta_aleatoria = random.randint(0,51)
+
+    data_J1 = []
 
     mano_J1 = dict_J1["mano_J1"]
 
@@ -236,7 +262,7 @@ def jugar_extra(dict_J1:dict) -> dict:
 
         cant_ace = mano_J1.count("A")
         if cant_ace >= 1:
-            mano_J1 = convert_ace(mano_J1)
+            mano_J1 = convert_ace(mano_J1, jug1, dir_bitacora)
 
         total = sumar_mano(mano_J1)
         data_J1 = {
@@ -246,17 +272,104 @@ def jugar_extra(dict_J1:dict) -> dict:
     }  
     return data_J1
 
-def obtener_resultado(total_pts_J1:int, total_pts_J2:int) -> str:
+def jugar_extra_2(dict_J2:dict, dir_bitacora: str) -> dict:
+    """
+    función: jugar_extra_1()
+    descripción: Jugar una carta extra del jugador 1
+    params: dict_J2
+    """
+    baraja_actual = dict_J2["baraja_actual"]
+    carta_aleatoria = random.randint(0,51)
+
+    data_J2 = []
+
+    mano_J2 = dict_J2["mano_J2"]
+
+    if dict_J2["baraja_actual"][carta_aleatoria] == 0:
+        ""
+    else:
+        mano_J2.append(dict_J2["baraja_actual"][carta_aleatoria])
+        baraja_actual[carta_aleatoria] = 0
+        print_mano(jug2, mano_J2)
+
+        cant_ace = mano_J2.count("A")
+        if cant_ace >= 1:
+            mano_J2 = convert_ace(mano_J2, jug2, dir_bitacora)
+
+        total = sumar_mano(mano_J2)
+        data_J2 = {
+            "total_mano_J2": total,
+            "baraja_actual": baraja_actual,
+            "mano_J2": mano_J2
+    }  
+    return data_J2
+
+def obtener_resultado(total_pts_J1:int, total_pts_J2:int, dir_bitacora:str) -> str:
+    """
+    función: obtener_resultado()
+    descripción: Función para obtener el mensaje de acuerdo al resultado obtenido
+    params: total_pts_J1, total_pts_J2
+    """
     msj = ""
     if total_pts_J1 == total_pts_J2:
         msj = "Esto es un empate"
+        msj_bitacora = f"El {jug1} y el {jug2} han empatado!\n"
+
     else:
         if total_pts_J1 > total_pts_J2:
             msj = f"Ganador del juego: {jug1}"
+            msj_bitacora = f"El {jug1} ha ganado y el {jug2} perdió el juego!\n"
         else:
             msj = f"Ganador del juego: {jug2}"
+            msj_bitacora = f"El {jug2} ha ganado y el {jug1} perdió el juego!\n"
+
+    agregar_accion_bitacora(dir_bitacora, msj_bitacora)
 
     return msj
+
+def random_boolean() -> bool:
+    """
+    función: random_boolean()
+    descripción: Función para obtener un valor aleatorio de True o False
+    params: N/A
+    """
+    random_num = random.random()
+
+    return random_num <= 0.4
+
+def crear_bitacora() -> str:
+    """
+    función: random_boolean()
+    descripción: Función para obtener un valor aleatorio de True o False
+    params: N/A
+    """
+    directory = "/Users/robjimn/Documents/Roberto Rojas/Cenfotec/Principios Programación/Proyecto/bitacora"
+
+    tiempo_actual = datetime.now()
+
+    # Crear String de timestamp
+    timestamp_str = tiempo_actual.strftime("%d%m%Y_%H%M%S")
+
+    file_name = f"Blackjack_Bitácora_{timestamp_str}.txt"
+
+    file_path = os.path.join(directory, file_name)
+
+    with open(file_path, "w") as file:
+        file.write(f"Bitácora: {tiempo_actual}\nPD: Las horas se muestran en formato de 24 horas\n")
+
+    return file_path
+
+def agregar_accion_bitacora(dir_bitacora, msj_bitacora):
+
+    ta = datetime.now()
+    formatted_time = ta.strftime("%I:%M:%S %p")
+    msj_completo = f"- {formatted_time} - {msj_bitacora}\n"
+
+    with open(dir_bitacora, "a") as file:
+        file.write(msj_completo)
+
+
+# /Users/robjimn/Documents/Roberto Rojas/Cenfotec/Principios Programación/Proyecto/bitacora
 
 # -- FUNCIONES DE PRUEBAS
 
--- Code/black_jack_main.py
+++ Code/black_jack_main.py
@@ -1,6 +1,10 @@
 from black_jack_functions import *
 import random
 
+# Crear archivo .txt para bitácora de acciones
+dir_bitacora = crear_bitacora()
+
+
 # Inicialización de Variables
 
 # Diccionarios
@@ -26,42 +30,102 @@ total_pts_J1 = 0
 total_pts_J2 = 0
 ronda = 1
 
+
 while salida_juego == False:
 
     print(f"\nRonda #{ronda}")
     input("Presione enter para iniciar\n")
 
+    msj_bitacora = f"\n\nSe ha iniciado un juego nuevo - Ronda No.{ronda}.\n"
+    agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
+
     cant_zeros = baraja.count(0)
+
+    total_J1 = 0
+    total_J2 = 0
+
+    if (total_J1 == 21 and cant_zeros < 50) or (total_J2 == 21 and cant_zeros < 50):
+        while total_J1 != 21 or total_J2 != 21:
+            print()
 
     if cant_zeros <= 48:
         #Parte del jugador 1
-        data_J1 = play_J1(baraja)
+        data_J1 = play_J1(baraja, dir_bitacora)
         print("Total: {}".format(data_J1["total_mano_J1"]))
         # print("Total: " + str(data_J1["total_mano_J1"])) - Qué es la diferencia o ventaja entre una y otra?
         baraja = data_J1["baraja_actual"]
         total_J1 = data_J1["total_mano_J1"]
+        cant_zeros = baraja.count(0)
 
         if total_J1 <= 20:
             extra = input("Jugador 1 decida - 1: Entra   2: Se queda\n")
             if extra == '1':
 
+                msj_bitacora = f"El {jug1} ha solicitado una carta adicional.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
                 cant_zeros = baraja.count(0)
                 
                 if cant_zeros <= 49:
-                    data_J1 = jugar_extra(data_J1)
+                    data_J1 = jugar_extra_1(data_J1, dir_bitacora)
                     print("Total: {}".format(data_J1["total_mano_J1"]))
                     baraja = data_J1["baraja_actual"]
+                    cant_zeros = baraja.count(0)
                 else: 
                     print("No es posible jugar la carta extra. Quedan solamente " + str((52-cant_zeros)) + " cartas disponibles para el " + jug2 +".\n")
 
         #Parte del jugador 2 - Máquina
-        data_J2 = play_J2(baraja)
+        data_J2 = play_J2(baraja, dir_bitacora)
         print("Total: {}".format(data_J2["total_mano_J2"]))
         baraja = data_J2["baraja_actual"]
+        total_J2 = data_J2["total_mano_J2"]
+        cant_zeros = baraja.count(0)
+
+        while total_J2 > 0 and total_J2 <= 9:
+            data_J2 = jugar_extra_2(data_J2, dir_bitacora)
+            print("Total: {}".format(data_J2["total_mano_J2"]))
+            baraja = data_J2["baraja_actual"]
+            cant_zeros = baraja.count(0)
+            
+            msj_bitacora = f"El {jug2} ha solicitado una carta adicional.\n"
+            agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
+        while total_J2 > 9 and total_J2 <= 14 and total_J1 > 14 and total_J2 <= 21:
+            data_J2 = jugar_extra_2(data_J2, dir_bitacora)
+            total_J2 += data_J2["total_mano_J2"]
+            print("Total: {}".format(data_J2["total_mano_J2"]))
+            baraja = data_J2["baraja_actual"]
+            cant_zeros = baraja.count(0)
+            
+            msj_bitacora = f"El {jug2} ha solicitado una carta adicional.\n"
+            agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+        
+        if total_J2 > 14 and total_J2 < 21 and total_J1 <= 21:
+            valor_random = random_boolean()
+
+            if valor_random == True:
+                data_J2 = jugar_extra_2(data_J2, dir_bitacora)
+                print("Total: {}".format(data_J2["total_mano_J2"]))
+                baraja = data_J2["baraja_actual"]
+                cant_zeros = baraja.count(0)
+                
+                msj_bitacora = f"El {jug2} ha solicitado una carta adicional.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
+            else:
+                msj_bitacora = f"El {jug2} ha decidido no solicitar mas cartas.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
 
         totales = total_pts(data_J1["total_mano_J1"], data_J2["total_mano_J2"])
         puntos_J1 = totales['pts_J1']
         puntos_J2 = totales['pts_J2']
+        
+        msj_bitacora = f"- Puntos del {jug1}: {puntos_J1}.\n- Puntos del {jug2}: {puntos_J2}.\n"
+        agregar_accion_bitacora(dir_bitacora, msj_bitacora)
+
+        print
         print(f"\n- Puntos de la mano del jugador 1: {puntos_J1}")
         print(f"- Puntos de la mano del jugador 2: {puntos_J2}\n")
 
@@ -73,30 +137,42 @@ while salida_juego == False:
             if opcion_salida == "1":
                 salida_juego = False
                 defect_val = False
+                
+                msj_bitacora = f"El {jug1} ha decidido continuar con el juego.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
 
             elif opcion_salida == "2":
                 salida_juego = True
                 defect_val = False
+                
+                msj_bitacora = f"El {jug1} ha decidido terminar el juego.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
 
             elif opcion_salida == "!" or opcion_salida == "@" or opcion_salida == "#" or opcion_salida == "$" or opcion_salida == "%" or opcion_salida == "^" or opcion_salida == "&" or opcion_salida == "*":
                 salida_juego = True
                 defect_val = False
                 print_baraja(baraja)
                 print("")
+                
+                msj_bitacora = f"El {jug1} ha solicitado mostrar la baraja del juego actual.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
             
             else:
                 defect_val = True
                 print("\nValor incorrecto! Trate nuevamente.")
+                
+                msj_bitacora = f"El {jug1} ha ingresado un valor incorrecto en la decisión de continuar o no con el juego.\n"
+                agregar_accion_bitacora(dir_bitacora, msj_bitacora)
 
         total_pts_J1 += puntos_J1
         total_pts_J2 += puntos_J2
     
     else:
         if cant_zeros == 50 or cant_zeros <= 49:
-            data_J1 = jugar_extra(data_J1)
+            data_J1 = jugar_extra_1(data_J1)
             print("Total: {}".format(data_J1["total_mano_J1"]))
 
-            data_J2 = jugar_extra(data_J2)
+            data_J2 = jugar_extra_2(data_J2)
             print("Total: {}".format(data_J2["total_mano_J2"]))
 
 
@@ -118,13 +194,18 @@ while salida_juego == False:
             print("No quedan mas cartas en la baraja. Inicie un nuevo juego. Gracias, chao!")
             salida_juego = True
             
-    
     ronda += 1
 
 print(f"\n- Puntos del {jug1}: {total_pts_J1}")
 print(f"- Puntos del {jug2}: {total_pts_J2}")
 
-msj = obtener_resultado(total_pts_J1, total_pts_J2)
+cartas_rest = 52 - cant_zeros
+print(f"\n- Cantidad de zeros: {cant_zeros}")
+print(f"- Total de cartas restantes: {cartas_rest}")
+print("\nBaraja actual:\n")
+print_baraja(data_J2["baraja_actual"])
+
+msj = obtener_resultado(total_pts_J1, total_pts_J2, dir_bitacora)
 
 print("")
 print(msj)
